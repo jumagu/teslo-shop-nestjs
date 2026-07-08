1af663676129c6f510c75607dd004c978cc6f441
jumagu <jmgg.gutierrez@gmail.com>
2026-07-08 13:03:02 -0500

    feat: generate product module and define product entity

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('text', { unique: true })
+  title: string;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+}
